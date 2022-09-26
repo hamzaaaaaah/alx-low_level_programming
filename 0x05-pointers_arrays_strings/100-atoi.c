609ec702e7e185b63d47a74d1ca56a15f04a531e
hamzaaaaaah <hamzaumarali@yahoo.com>
2022-09-26 09:24:59 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - Converts a string to an integar.
+ * @s: The string to be converted.
+ *
+ * Return: The integar value of the converted string.
  */
 int _atoi(char *s)
 {
+	int sign = 1;
+	unsigned int number = 0;
 
-	return (0);
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
