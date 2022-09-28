bcf425e8b1d9b6ef42d8d8e256dbffb7fe25b5af
hamzaaaaaah <hamzaumarali@yahoo.com>
2022-09-28 16:45:06 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,53 +1,94 @@
+/*
+ * File: 101-wildcmp.c 
+ * Auth: sam 
+ */
+
 #include "main.h"
 #include <stdio.h>
-/**
- * is_palindrome - Entry point
- * @s: int value
- * Return: Return .
- */
-int is_palindrome(char *s)
-{
-	int a;
 
-	a = rev(s);
-	if (a <= 1)
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+ * strlen_no_wilds - Return the length of a string, ignore wildcard character
+ * @str: The string to be measured.
+ *
+ * Return: The length.
+ */
+int strlen_no_wilds(char *str) 
+{
+	int len = 0, index = 0; 
+
+	if (*(str + index))
 	{
-		return (1);
+		if (*str != '*')
+			len++;
+
+		index++;
+		len += strlen_no_wilds(str + index);
 	}
-	return (rev1(s, a));
+
+	return (len);
+}
+
+/**
+ * iterate_wild - Iterates through a string in a wldcd until points not wldcd
+ * @wildstr: The string to be iterated through.
+ */
+void iterate_wild(char **wildstr)
+{
+	if (**wildstr == '*')
+	{
+		(*wildstr)++;
+		iterate_wild(wildstr);
+	}
 }
 /**
- * rev- Entry point
- * @s: int value
- * Return: return.
+ * postfix_match - Checks if a string str matches the postfix of anoth string
+ * @str: The string to be matched.
+ * @postfix: The postfix.
+ *
+ * Return: If str and postfix are identical - pointer to the null byte end pf
  */
-int rev(char *s)
+char *postfix_match(char *str, char *postfix)
 {
-	if (*s == '\0')
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
+
+	if (*postfix == '*')
+		iterate_wild(&postfix);
+
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0');
 	{
-	return (0);
+		postfix++;
+		return (postfix_match(str, postfix));
 	}
-	return (1 + rev(s + 1));
+
+	return (postfix);
 }
 /**
- * rev1 - Entry point
- * @s: int value
- * @l: int value
- * Return: check.
- */
-int rev1(char *s, int l)
+* wildcmp - Compares two strings, considering wildcard characters.
+* @s1: The first string to be compared.
+* @s2: The second string to be compared - may contain wildcards.
+*
+* Return: If the strings can be considered identical - 1.
+*         Otherwise - 0.
+*/
+int wildcmp(char *s1, char *s2)
 {
-	if (l <= 1)
+	if (*s2 == '*')
 	{
-		return (1);
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
 	}
-	if (*s != s[l - 1])
-	{
+
+	if (*s2 == '\0')
+		return (1);
+
+	if (*s1 != *s2)
 		return (0);
-	}
-	else
-	{
-	return (rev1(s + 1, l - 2));
-	}
-}
 
+	return (wildcmp(++s1, ++s2));
+}
