316746bfa76740b653f0304faa33f0e62a908af3
hamzaaaaaah <hamzaumarali@yahoo.com>
2022-09-29 07:47:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
-
- * main - prints a text
+ * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful,\" - Dora Korpar, 2015-10-19\n", 59);
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(2, s, 59);
 	return (1);
-
 }
