7fe266d031c27ead931dcbcc1814a6d32aaa5190
hamzaaaaaah <hamzaumarali@yahoo.com>
2022-10-04 13:50:37 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memset - fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte b
+ * @n: first n bytes of memory area pointed by s
  *
- * Return: Nothing.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+	for (i = 0; i < n; i++, s++)
+		*s = b;
+	s = s - n;
+	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,49 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#endif
 
+#ifndef _MEMSET_H
+#define _MEMSET_H
+char *_memset(char *s, char b, unsigned int n);
+#endif
+
+#ifndef _MEMCPY_H
+#define _MEMCPY_H
+char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
+
+#ifndef _STRCHR_H
+#define _STRCHR_H
+char *_strchr(char *s, char c);
+#endif
+
+#ifndef _STRSPN_H
+#define _STRSPN_H
+unsigned int _strspn(char *s, char *accept);
+#endif
+
+#ifndef _STRPBRK_H
+#define _STRPBRK_H
+char *_strpbrk(char *s, char *accept);
+#endif
+
+#ifndef _STRSTR_H
+#define _STRSTR_H
+char *_strstr(char *haystack, char *needle);
+#endif
+
+#ifndef _PRINT_CHESSBOARD_H
+#define _PRINT_CHESSBOARD_H
+void print_chessboard(char (*a)[8]);
+#endif
+
+#ifndef _PRINT_DIAGSUMS_H
+#define _PRINT_DIAGSUMS_H
+void print_diagsums(int *a, int size);
+#endif
+
+#ifndef _SET_STRING_H
+#define _SET_STRING_H
+void set_string(char **s, char *to);
 #endif
