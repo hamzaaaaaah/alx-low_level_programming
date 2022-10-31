b31b3d7460a1a21bfcef4a40bb94aedf68fc1cd3
hamzaaaaaah <hamzaumarali@yahoo.com>
2022-10-31 12:56:17 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - Entry Point
+ * @head: head
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	if (head == NULL)
+		exit(98);
+
+	return (0);
+}
